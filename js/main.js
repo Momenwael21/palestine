5692778e3860001c48f00d1a668f9f97d715ea6b
Momenwael21 <waelmomen42@gmail.com>
2022-10-27 15:22:21 +0200

    step-2

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ let sliderWrapper = document.querySelector(".landing .slider .slider-wrapper");
 let prevtBtn = document.querySelector(".landing .slider .controls .prev");
 let nextBtn = document.querySelector(".landing .slider .controls .next");
 
-let indexOfQuote = 1;
-
+// turn on slider
+let indexOfQuote = 0;
 nextBtn.addEventListener("click", function () {
   indexOfQuote == 5 ? (indexOfQuote = 0) : indexOfQuote++;
   moveSlider(indexOfQuote);
@@ -14,6 +14,40 @@ prevtBtn.addEventListener("click", function () {
   moveSlider(indexOfQuote);
 });
 
+function startSlider() {
+  if (indexOfQuote == 5) {
+    indexOfQuote = 0;
+  } else {
+    indexOfQuote++;
+  }
+  moveSlider(indexOfQuote);
+}
+
+setInterval(startSlider, 5000);
+
 function moveSlider(index) {
   sliderWrapper.style.marginLeft = `${-index * 300}px`;
 }
+
+// turn on the map
+let mapBtn = document.querySelector("header .toggler");
+let mapSection = document.querySelector(".map");
+
+mapBtn.addEventListener("click", () => {
+  if (mapSection.classList.contains("active")) {
+    mapSection.classList.remove("active");
+  } else {
+    mapSection.classList.add("active");
+  }
+});
+
+// play audio when click open button
+let openBtn = document.querySelector("a.open");
+let audioPlayer = document.querySelector(".cause audio");
+
+openBtn.addEventListener("click", () => {
+  audioPlayer.setAttribute(
+    "src",
+    "../audio/وقضينا إلى بني إسرائيل في الكتاب لتفسدن في الأرض مرتين ولتعلن علوا كبيرا خالد الجليل [TubeRipper.com].webm"
+  );
+});
